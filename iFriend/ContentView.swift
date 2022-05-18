dd94a6bf29669d3732b878003d427ec9cea1d3e8
Lucas Chae <hi@lucaschae.com>
2022-05-18 16:47:55 +0900

    Initial Commit

---
+++ iFriend/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iFriend
+//
+//  Created by Lucas Chae on 5/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iFriend/iFriendApp.swift
@@ -0,0 +1,17 @@
+//
+//  iFriendApp.swift
+//  iFriend
+//
+//  Created by Lucas Chae on 5/18/22.
+//
+
+import SwiftUI
+
+@main
+struct iFriendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
